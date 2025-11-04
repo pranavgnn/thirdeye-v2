4f47ac2f11949f6a97b7df190e882b25062a6010
Aprameya <aprameya.mitmpl2023@learner.manipal.edu>
2025-11-04 09:36:44 +0530

    db fix

--- database/init.sql
+++ database/init.sql
@@ -3,6 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 \i /docker-entrypoint-initdb.d/migrations/001-auth-schema.sql
 \i /docker-entrypoint-initdb.d/migrations/002-violations-schema.sql
-\i /docker-entrypoint-initdb.d/migrations/003-audit-schema.sql
+\i /docker-entrypoint-initdb.d/migrations/003-audit.schema.sql
 \i /docker-entrypoint-initdb.d/migrations/004-vectors-schema.sql
 \i /docker-entrypoint-initdb.d/migrations/005-motor-vehicle-act.sql
+\i /docker-entrypoint-initdb.d/migrations/006-analysis-sessions.sql
+\i /docker-entrypoint-initdb.d/migrations/007-seed-admin.sql
